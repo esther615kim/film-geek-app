fda72ad561f622c61c78723fd33a379df080e5a4
zakt1 <you@example.com>
2022-02-22 15:59:06 +0000

    config: firebase config & prettier

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,14 +1,13 @@
 // Import the functions you need from the SDKs you need
-import firebase from 'firebase/app';
+import firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA355ud0O4DdsfsdyvutbMo1rlZJUy63jB8nxnE',
-  authDomain: 'codequizapp-fsddfsdfdsf39a9.firebaseapp.com',
-  projectId: 'codequizapp-sdfdsff39asdfs9',
-  storageBucket: 'codequidsfsfsdfdszapp-f39a9.appspot.com',
-  messagingSenderId: '1096dfsdf252670419',
-  appId: '1:109625267041sdfds9:web:d47e4d7af892327d2eef26',
-  measurementId: 'G-FNHWY58JE8',
+  apiKey: "AIzaSyAjD5Tmf-_rPPs4LRoq9hfJlKal95lVgss",
+  authDomain: "filmgeek-b6c1a.firebaseapp.com",
+  projectId: "filmgeek-b6c1a",
+  storageBucket: "filmgeek-b6c1a.appspot.com",
+  messagingSenderId: "265459074244",
+  appId: "1:265459074244:web:02df062bfc6c961bb7b591",
 };
 
 // Initialize Firebase
