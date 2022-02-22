7163cc5a51cb46c8ab97c35ca1dbdfde7c1db45a
zakt1 <you@example.com>
2022-02-22 15:44:45 +0000

    config: initial project setup with dependencies

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA355ud0O4DdsfsdyvutbMo1rlZJUy63jB8nxnE',
+  authDomain: 'codequizapp-fsddfsdfdsf39a9.firebaseapp.com',
+  projectId: 'codequizapp-sdfdsff39asdfs9',
+  storageBucket: 'codequidsfsfsdfdszapp-f39a9.appspot.com',
+  messagingSenderId: '1096dfsdf252670419',
+  appId: '1:109625267041sdfds9:web:d47e4d7af892327d2eef26',
+  measurementId: 'G-FNHWY58JE8',
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
