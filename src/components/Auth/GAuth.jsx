45d2b40db7a58e3750297be3c702ba67fd1e8af0
CrypticSignal <hshafiq@hotmail.co.uk>
2022-02-24 12:31:31 +0000

    fix: syntax related errors

--- src/components/Auth/GAuth.jsx
+++ src/components/Auth/GAuth.jsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import { Avatar } from "react-native-paper";
+import { View } from "react-native";
 const G_LOGO = "../../../assets//GLogo.png";
 
 const GAuth = () => {
   return (
-    <div>
+    <View>
       {/* <Avatar.Image size={60} source={require(G_LOGO)} /> */}
-      <Avatar.Icon size={60} icon="google"  />
-    </div>
+      <Avatar.Icon size={60} icon="google" />
+    </View>
   );
 };
 
