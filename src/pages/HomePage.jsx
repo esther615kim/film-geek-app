b0d2324e43624adea8b6aebab85d3fea562e5ec4
CrypticSignal <theaudiophile@outlook.com>
2022-02-25 12:01:38 +0000

    first commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase/config";
+import { useEffect } from "react";
 
 const HomePage = () => {
+  const [moviesData, setMoviesData] = useState({});
+
+  useEffect(() => {
+    const docRef = doc(db, "moviesData", "oXH9AxZ3Qby7qmMt0DGq");
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      setMoviesData(docSnap.data().results);
+      console.log(moviesDadocSnap.data().results);
+    }
+  }, []);
+
   return (
     <View>
       <Text>HomePage</Text>
