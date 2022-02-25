4fd5fdd5fe172484081f793ed932d29dde46e0e3
CrypticSignal <theaudiophile@outlook.com>
2022-02-25 16:57:24 +0000

    feat: show list of movies

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,56 @@
-import React from "react";
-import { Text, View } from "react-native";
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "../../firebase/config";
+import { useState } from "react";
+import { Text, View, ScrollView } from "react-native";
+import { doc, getDoc, addDoc, collection } from "firebase/firestore";
+import { db } from "../firebase/config";
 import { useEffect } from "react";
+import { Card, Title, Paragraph } from "react-native-paper";
+
+import { LogBox } from "react-native";
+
+LogBox.ignoreLogs(["Setting a timer"]);
+
+const handlePress = (movieName) => {
+  console.log(movieName);
+};
 
 const HomePage = () => {
-  const [moviesData, setMoviesData] = useState({});
+  const [moviesData, setMoviesData] = useState([]);
 
   useEffect(() => {
-    const docRef = doc(db, "moviesData", "oXH9AxZ3Qby7qmMt0DGq");
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-      setMoviesData(docSnap.data().results);
-      console.log(moviesDadocSnap.data().results);
+    async function getMovieData() {
+      console.log("fetching");
+      const docRef = doc(db, "movieData", "qWEaphXhRnZGlArWxqIo");
+      const docSnap = await getDoc(docRef);
+      console.log("fetched");
+      if (docSnap.exists()) {
+        setMoviesData(docSnap.data().movies);
+      }
     }
+    getMovieData();
   }, []);
 
   return (
-    <View>
-      <Text>HomePage</Text>
-    </View>
+    <ScrollView>
+      {moviesData.length !== 0 ? (
+        moviesData.map((movie) => (
+          <View key={movie.id}>
+            <Card onPress={() => handlePress(movie.title)}>
+              <Card.Cover
+                source={{
+                  uri: movie.posterUrl,
+                }}
+              />
+              <Card.Content>
+                <Title>{movie.title}</Title>
+                <Paragraph>{movie.plot}</Paragraph>
+              </Card.Content>
+            </Card>
+          </View>
+        ))
+      ) : (
+        <Text>Loading movies</Text>
+      )}
+    </ScrollView>
   );
 };
 
