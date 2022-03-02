52ed4f5bcd8ed8af4a9af2863ce517b0617e9a2e
esther615kim <vilchen@naver.com>
2022-03-03 08:38:23 +0900

    style: Quiz Page

--- src/components/Quizzes/MultipleChoice.jsx
+++ src/components/Quizzes/MultipleChoice.jsx
@@ -12,6 +12,7 @@ import {
   Modal,
 } from "react-native";
 import React, { useEffect, useState, useReducer } from "react";
+import { Divider, Subheading } from 'react-native-paper';
 import { getFirestore, doc, getDoc } from "firebase/firestore";
 import { db } from "../../firebase/config";
 import { Button } from "react-native-paper";
@@ -36,7 +37,7 @@ export default function MultipleChoice() {
 
   const [numCorrectAnswers, setNumCorrectAnswers] = useState(0);
 
-  const [questCount, setQuestCount] = useState(0); // <-- increment 'onNext'
+  const [questCount, setQuestCount] = useState(1); // <-- increment 'onNext'
   const [scoreCount, setScoreCount] = useState(0); // <--- inc/decrement 'onNext'
   const [currQuestion, setCurrQuestion] = useState([]);
   const [currAnswer, setCurrAnswer] = useState("");
@@ -111,10 +112,11 @@ export default function MultipleChoice() {
     const destructured = formattedOptns;
 
     const highlight = (itemId, event) => {
-      myRefs.current[itemId].setNativeProps({ style: { backgroundColor: "blue" } });
+      myRefs.current[itemId].setNativeProps({ style: { border:"2px solid #58e065" } });
     };
 
     return (
+      <View style={{ backgroundColor: "#0a152b", color: "#fff" }}>
       <TouchableOpacity
         onPress={() => {
           updateState(formattedOptns.id);
@@ -125,6 +127,7 @@ export default function MultipleChoice() {
           <Text style={styles.listText}>{formattedOptns.option}</Text>
         </View>
       </TouchableOpacity>
+      </View>
     );
   };
 
@@ -153,7 +156,7 @@ export default function MultipleChoice() {
     };
 
     return (
-      <View>
+      <View style={{marginTop:20, background:"pink"}}>
         <Button onPress={increaseQCount}>{nextButtonTitle}</Button>
         <Button onPress={resetQCount}>{"Restart quiz"}</Button>
       </View>
@@ -162,9 +165,9 @@ export default function MultipleChoice() {
 
   const ResultsModal = () => {
     return (
-      <View>
+      <View style={{ backgroundColor: "#0a152b", color: "#fff" }}>
         <Modal visible={modalOpen} animationType="slide">
-          <View>
+          <View color={{background:"#fff"}}>
             <AntDesign
               name="close"
               size={24}
@@ -199,9 +202,9 @@ export default function MultipleChoice() {
   };
 
   return (
-    <SafeAreaView>
-      <Button>{"hi"}</Button>
-
+    <SafeAreaView style={{ backgroundColor: "#0a152b"}}>
+       <Subheading style={{fontSize:22,fontWeight:600, color:"#fff",paddingBottom:60,paddingTop:60, textAlign:"center", paddingBottom:5}}>Question {questCount}
+       <Text style={{color:"#ccc",fontSize:14}}> /10</Text></Subheading>
       <FlatList
         data={currOptions}
         renderItem={renderzItem}
@@ -226,26 +229,27 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   listText: {
-    color: "white",
+    color: "#fff",
+    fontSize:15,
+    padding:10
   },
   listHeaderText: {
-    color: "black",
+    color: "#ccc",
+    fontSize:20,
+    fontWeight:600
   },
   listItem: {
     flex: 1,
-    marginRight: 20,
-    marginLeft: 20,
-    marginTop: 10,
-    backgroundColor: "#9575cd",
+    marginRight: 25,
+    marginLeft: 25,
+    marginTop: 15,
+    border:"1px solid #eee",
     padding: 10,
-    borderRadius: 5,
+    borderRadius: 15,
   },
   listHeader: {
     flex: 1,
-    marginRight: 20,
-    marginLeft: 20,
-    marginTop: 10,
-    backgroundColor: "#2196f3",
+    margin:20,
     padding: 10,
     borderRadius: 5,
   },
